52d4c6028ca37772cf51e0457ad8dd1485fc2c98
Alexis Rosuel <rosuelalexis1@gmail.com>
2019-10-22 13:39:25 +0200

    rename

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='phd_utils',
+     name='phdutils',
      version='0.2',
      author="Alexis Rosuel",
      author_email="rosuelalexis1@gmail.com",
      description="phd useful methods",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/alexisrosuel/phd_utils",
+     url="https://github.com/alexisrosuel/phdutils",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
